58ffdd701d4475f3ca4a25e79b68f7c155528f00
shierjiu <1615834518@qq.com>
2025-03-23 01:57:39 +0800

    3-23

--- src/views/user/indexV.vue
+++ src/views/user/indexV.vue
@@ -26,7 +26,7 @@ const logout = async () => {
         <el-menu router :default-active="$route.path">
           <el-menu-item index="/user/getnum">预约挂号</el-menu-item>
           <el-menu-item index="/user/appointment">我的预约</el-menu-item>
-          <el-menu-item index="/user/case">我的病例</el-menu-item>
+          <el-menu-item index="/user/mycase">我的病例</el-menu-item>
           <el-menu-item index="/user/personalinformation">个人信息</el-menu-item>
         </el-menu>
       </el-aside>
